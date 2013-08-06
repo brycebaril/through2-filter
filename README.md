803133e1361847a0f95260298184314f7eaa8d8e
Bryce Baril <bryce@ravenwall.com>
2013-08-05 21:43:50 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,54 @@
+through2-filter
+===============
+
+This is a super thin wrapper around [through2](http://npm.im/through2) that works like `Array.prototype.filter` but for streams.
+
+For when through2 is just too verbose :wink:
+
+Note you will **NOT** be able to alter the content of the chunks. This is intended for filtering only. If you want to modify the stream content, use either `through2` or `through2-map`.
+
+```js
+var filter = require("through2-filter")
+
+var skip = filter(function (chunk) {
+  // skip buffers longer than 100
+  return chunk.length < 100
+})
+
+// vs. with through2:
+var skip = through2(function (chunk, encoding, callback) {
+  // skip buffers longer than 100
+  if (chunk.length < 100) this.push(chunk)
+  return callback()
+})
+
+// Then use your filter:
+source.pipe(skip).pipe(sink)
+
+// Additionally accepts `wantStrings` argument to conver buffers into strings
+var alphanum = new RegExp("^[A-Za-z0-1]+$")
+var scrub = filter({wantStrings: true}, function (str) {
+  return alphanum.exec(str)
+})
+
+// Works like `Array.prototype.filter` meaning you can specify a function that
+// takes up to two* arguments: fn(element, index)
+var skip10 = filter(function (element, index) {
+  return index > 10
+})
+```
+
+*Differences from `Array.prototype.filter`:
+  * No third `array` callback argument. That would require realizing the entire stream, which is generally counter-productive to stream operations.
+  * `Array.prototype.filter` doesn't modify the source Array, which is somewhat nonsensical when applied to streams.
+
+Options
+-------
+
+  * wantStrings: Automatically call chunk.toString() for the super lazy.
+  * all other through2 options
+
+LICENSE
+=======
+
+MIT
---
+++ test/index.js
@@ -0,0 +1,164 @@
+var test = require("tape").test
+
+var filter = require("../")
+var spigot = require("stream-spigot")
+var concat = require("concat-stream")
+
+test("ctor", function (t) {
+  t.plan(2)
+
+  var Filter = filter.ctor(function (record) {
+    return !record.skip
+  })
+
+  function combine(records) {
+    t.equals(records.length, 3, "Correct number of remaining records")
+    t.notOk(records.filter(function (r) { return r.skip }).length, "No remaining skip records")
+  }
+
+  spigot([
+    {foo: "bar"},
+    {foo: "baz", skip: true},
+    {foo: "bif", skip: true},
+    {foo: "blah"},
+    {foo: "buzz"},
+  ], {objectMode: true})
+    .pipe(new Filter({objectMode: true}))
+    .pipe(concat(combine))
+})
+
+test("ctor options", function (t) {
+  t.plan(2)
+
+  var Filter = filter.ctor({objectMode: true}, function (record) {
+    return !record.skip
+  })
+
+  function combine(records) {
+    t.equals(records.length, 3, "Correct number of remaining records")
+    t.notOk(records.filter(function (r) { return r.skip }).length, "No remaining skip records")
+  }
+
+  spigot([
+    {foo: "bar"},
+    {foo: "baz", skip: true},
+    {foo: "bif", skip: true},
+    {foo: "blah"},
+    {foo: "buzz"},
+  ], {objectMode: true})
+    .pipe(new Filter())
+    .pipe(concat(combine))
+})
+
+test("ctor buffer wantStrings", function (t) {
+  t.plan(1)
+
+  var re = new RegExp("skip")
+  var Filter = filter.ctor({wantStrings: true}, function (chunk) {
+    return chunk.length <= 5
+  })
+
+  function combine(result) {
+    t.equals(result.toString(), "abuvwxyz", "result is correct")
+  }
+
+  spigot([
+    "a",
+    "b",
+    "cskipk",
+    "lmnopqrstskip",
+    "u",
+    "vwxyz",
+  ]).pipe(new Filter())
+    .pipe(concat(combine))
+})
+
+test("simple", function (t) {
+  t.plan(2)
+
+  var f = filter({objectMode: true}, function (record) {
+    return !record.skip
+  })
+
+  function combine(records) {
+    t.equals(records.length, 3, "Correct number of remaining records")
+    t.notOk(records.filter(function (r) { return r.skip }).length, "No remaining skip records")
+  }
+
+  spigot([
+    {foo: "bar"},
+    {foo: "baz", skip: true},
+    {foo: "bif", skip: true},
+    {foo: "blah"},
+    {foo: "buzz"},
+  ], {objectMode: true})
+    .pipe(f)
+    .pipe(concat(combine))
+})
+
+test("simple buffer", function (t) {
+  t.plan(1)
+
+  var f = filter({objectMode: true}, function (chunk) {
+    return chunk.length <= 5
+  })
+
+  function combine(result) {
+    t.equals(result.toString(), "abuvwxyz", "result is correct")
+  }
+
+  spigot([
+    "a",
+    "b",
+    "cdefghijk",
+    "lmnopqrst",
+    "u",
+    "vwxyz",
+  ]).pipe(f)
+    .pipe(concat(combine))
+})
+
+test("simple buffer wantStrings", function (t) {
+  t.plan(1)
+
+  var re = new RegExp("skip")
+  var f = filter({wantStrings: true}, function (chunk) {
+    return chunk.length <= 5
+  })
+
+  function combine(result) {
+    t.equals(result.toString(), "abuvwxyz", "result is correct")
+  }
+
+  spigot([
+    "a",
+    "b",
+    "cskipk",
+    "lmnopqrstskip",
+    "u",
+    "vwxyz",
+  ]).pipe(f)
+    .pipe(concat(combine))
+})
+
+test("simple index", function (t) {
+  t.plan(1)
+
+  var f = filter({objectMode: true}, function (record, index) {
+    return index < 2
+  })
+
+  function combine(records) {
+    t.deepEquals(records, [{foo: "bar"},{foo: "baz"}], "Expected content")
+  }
+
+  spigot([
+    {foo: "bar"},
+    {foo: "baz"},
+    {foo: "bif"},
+    {foo: "blah"},
+    {foo: "buzz"},
+  ], {objectMode: true})
+    .pipe(f)
+    .pipe(concat(combine))
+})
